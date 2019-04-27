91176e0e46d7f73c3ecd1dd45f01cd2e9ab95a73
Joseph Young <jsyoung0891@gmail.com>
2019-04-27 21:10:17 +0100

    made controller and views files

--- controllers/pokemon_controller.rb
+++ controllers/pokemon_controller.rb
@@ -1,2 +1,10 @@
 require('sinatra')
 require('sinatra/contrib/all')
+require_relative('../models/pokemon.rb')
+require_relative('../models/vet.rb')
+also_reload('../models/*')
+
+get '/pokemon' do
+  @pokemon = Pokemon.show_all()
+  erb(:index)
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ require('pry-byebug')
 
 vet1 = Vet.new({"name" => "joy"})
 vet1.name = "Nurse Joy"
-vet1.update()
+# vet1.update()
 # vet1.save()
 # p Vet.show_all()
 
