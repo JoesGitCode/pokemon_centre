b2694e1e5e9d814b3b6d13f31ac8a3e3cce7f93d
Joseph Young <jsyoung0891@gmail.com>
2019-07-22 18:20:49 +0100

    install latest gem versions

--- Gemfile
+++ Gemfile
@@ -1,11 +1,4 @@
-
 source 'https://rubygems.org'
-ruby '2.4.1'
 gem 'sinatra'
-gem 'sinatra-contrib'
-gem 'mustermann'
-gem 'rack'
-gem 'bundler'
-gem 'tilt'
-gem 'rack-protection'
-gem 'pg'
+gem 'pg', '~> 0.18'
+gem 'sinatra/contrib'
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,11 +4,11 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'dlian1l5mla13',
-                         host: 'ec2-23-21-160-38.compute-1.amazonaws.com
-',
-user: 'pcdezmcmattdmu',
-password: '3c60c5fb974d01c269b2f16a92ce90e4a870b162ead2d325445d449bbd0c53f3'
+      db = PG.connect({ dbname: 'pokemon_centre', host: 'localhost'
+        # dbname: 'dlian1l5mla13',
+                        # host: 'ec2-23-21-160-38.compute-1.amazonaws.com ',
+# user: 'pcdezmcmattdmu',
+# password: '3c60c5fb974d01c269b2f16a92ce90e4a870b162ead2d325445d449bbd0c53f3'
 })
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
