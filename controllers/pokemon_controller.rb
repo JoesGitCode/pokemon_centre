61b64a7fa3a03c6279dcb23a55cd9029c7447185
Joseph Young <jsyoung0891@gmail.com>
2019-08-07 12:54:30 +0100

    took out the contrib/all requires

--- controllers/pokemon_controller.rb
+++ controllers/pokemon_controller.rb
@@ -1,5 +1,5 @@
 require('sinatra')
-require('sinatra/contrib/all')
+# require('sinatra/contrib/all')
 require_relative('../models/pokemon.rb')
 require_relative('../models/vet.rb')
 also_reload('./models/*')
