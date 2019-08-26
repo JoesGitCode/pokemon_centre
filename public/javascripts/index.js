b3518fc9ed90eed9e99b4dccbb7c9ec09425433a
Joseph Young <jsyoung0891@gmail.com>
2019-08-26 17:37:26 +0100

    added js slider for mobile nav

---
+++ public/javascripts/index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("dom content loaded");
+
+  const navSlide = () => {
+    const burger = document.querySelector(".nav-burger");
+    const navbar = document.querySelector(".nav-buttons");
+
+    burger.addEventListener("click", () => {
+      navbar.classList.toggle("mobile-nav-on");
+    });
+  };
+
+  navSlide();
+});
